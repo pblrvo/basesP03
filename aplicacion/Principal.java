fc3d29c6891f2c5c579eba3da6b241fa63edb974
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2022-03-28 23:11:05 +0200

    ventana

---
+++ aplicacion/Principal.java
@@ -0,0 +1,10 @@
+package aplicacion;
+
+import java.sql.SQLException;
+import dominio.*;
+
+public class Principal {
+    public static void main(String[] args) throws SQLException {
+        Inicio inicio = new Inicio();
+    }
+}
