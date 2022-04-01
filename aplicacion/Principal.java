0526d636c6adfc7529eafbee27767a9777a3d69c
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2022-04-01 17:50:25 +0200

    correction

--- aplicacion/Principal.java
+++ aplicacion/Principal.java
@@ -9,14 +9,13 @@ import java.sql.SQLException;
 public class Principal {
     public static void main(String[] args) throws SQLException {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root",
                     "123");
             Statement statement = con.createStatement();
             long time_before = System.currentTimeMillis();
             ResultSet result = statement.executeQuery("show databases;");
-            long time_passed = time_before - System.currentTimeMillis();
-            System.out.println("Tiempo de consulta: " + time_passed / 100000000);
+            long time_passed = System.currentTimeMillis() - time_before;
+            System.out.println("Tiempo de consulta: " + time_passed / 100);
             System.out.println("Connected");
         } catch (Exception e) {
             System.out.println(e);
