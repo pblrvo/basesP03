c4057a737fd256e5e6d32d68057a2e1bbc19ba7b
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2022-04-01 17:14:11 +0200

    foundation

--- aplicacion/Principal.java
+++ aplicacion/Principal.java
@@ -1,10 +1,25 @@
 package aplicacion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.SQLException;
-import dominio.*;
 
 public class Principal {
     public static void main(String[] args) throws SQLException {
-        Inicio inicio = new Inicio();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root",
+                    "123");
+            Statement statement = con.createStatement();
+            long time_before = System.currentTimeMillis();
+            ResultSet result = statement.executeQuery("show databases;");
+            long time_passed = time_before - System.currentTimeMillis();
+            System.out.println("Tiempo de consulta: " + time_passed / 100000000);
+            System.out.println("Connected");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
