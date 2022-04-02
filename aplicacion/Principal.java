ef359388203ec6571835404ad0896d78d95e8619
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2022-04-02 17:48:47 +0200

    finished

--- aplicacion/Principal.java
+++ aplicacion/Principal.java
@@ -21,11 +21,25 @@ public class Principal {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root",
                     "123");
             Statement statement = con.createStatement();
+            // Apartado a ------------------------------------------------------
             tiempo(statement, "select avg(population) from world.city;");
             tiempo(statement, "select avg(population) from world.city;");
             tiempo(statement, "select count(*) from sakila.actor;");
             tiempo(statement, "select count(*) from sakila.actor;");
-            System.out.println("Connected");
+            // Apartado b ------------------------------------------------------
+            tiempo(statement, "select * from world.country;");
+            tiempo(statement, "select * from world.country;");
+            tiempo(statement, "select * from world.country where continent = 'Europe';");
+            tiempo(statement, "select * from world.country where continent = 'Europe';");
+            // Apartado c ------------------------------------------------------
+            tiempo(statement,
+                    "select actor.actor_id, actor.first_name, actor.last_name, film_actor.film_id, film.title from sakila.actor inner join film_actor on sakila.actor.actor_id = sakila.film_actor.actor_id inner join film on sakila.film.film_id = sakila.film_actor.film_id;");
+            tiempo(statement,
+                    "select actor.actor_id, actor.first_name, actor.last_name, film_actor.film_id, film.title from sakila.actor inner join film_actor on sakila.actor.actor_id = sakila.film_actor.actor_id inner join film on sakila.film.film_id = sakila.film_actor.film_id;");
+            tiempo(statement,
+                    "select city.ID, city.Name, city.District, city.Population, country.Name, country.Continent from world.city inner join world.country on city.CountryCode = country.Code;");
+            tiempo(statement,
+                    "select city.ID, city.Name, city.District, city.Population, country.Name, country.Continent from world.city inner join world.country on city.CountryCode = country.Code;");
         } catch (Exception e) {
             System.out.println(e);
         }
